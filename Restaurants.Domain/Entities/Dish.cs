a437835ae247ee5bdefa724cda7e0fdacaf1c335
PavelRahman404 <paveltauker@gmail.com>
2024-10-03 13:14:29 +0600

    Add project files.

---
+++ Restaurants.Domain/Entities/Dish.cs
@@ -0,0 +1,11 @@
+﻿namespace Restaurants.Domain.Entities
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Desciption { get; set; } = default!;
+        public decimal Price { get; set; }
+
+    }
+}
