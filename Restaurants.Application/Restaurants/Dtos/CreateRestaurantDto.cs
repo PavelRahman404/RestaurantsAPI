f56776c5a4454afb87b776bc0a5290a7229c86eb
PavelRahman404 <paveltauker@gmail.com>
2024-10-06 21:22:21 +0600

    Fluent validation setup

--- Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
+++ Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
@@ -4,23 +4,14 @@ namespace Restaurants.Application.Restaurants.Dtos
 {
     public class CreateRestaurantDto
     {
-        [StringLength(100,MinimumLength =3)]
         public string Name { get; set; } = default!;
         public string Description { get; set; } = default!;
-
-        [Required(ErrorMessage ="Insert a valid category")]
         public string Category { get; set; } = default!;
         public bool HasDelivery { get; set; } = default!;
-
-        [EmailAddress(ErrorMessage ="Please provide a valid email address")]
         public string? ContactEmail { get; set; }
-
-        [Phone(ErrorMessage ="Please provide a valid phone number")]
         public string? ContactNumber { get; set; }
-
         public string? City { get; set; }
         public string? Street { get; set; }
-
         public string? PostalCode { get; set; }
 
     }
---
+++ Restaurants.Application/Restaurants/Validators/CreateRestaurantDtoValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using Restaurants.Application.Restaurants.Dtos;
+using System.Collections.Generic;
+
+namespace Restaurants.Application.Restaurants.Validators
+{
+    public class CreateRestaurantDtoValidator : AbstractValidator<CreateRestaurantDto>
+    {
+        private readonly List<string> validCategories = ["Bangladeshi","Chinese","Japanese","korean","Mexican"];
+        public CreateRestaurantDtoValidator()
+        {
+            RuleFor(dto => dto.Name)
+                .Length(3,100);
+
+            RuleFor(dto => dto.Category)
+                .Must(validCategories.Contains)
+                .WithMessage("Invalid category. Please choose from the valid categories");
+
+            RuleFor(dto => dto.Description)
+                .NotEmpty().WithMessage("Description is required");
+
+            RuleFor(dto => dto.Category)
+                .NotEmpty().WithMessage("Insert a valid category");
+
+            RuleFor(dto => dto.ContactEmail)
+                .EmailAddress().WithMessage("Please provide a valid email address");
+
+        }
+    }
+}
