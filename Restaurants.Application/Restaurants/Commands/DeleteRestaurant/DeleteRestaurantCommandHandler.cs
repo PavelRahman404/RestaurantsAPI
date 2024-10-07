917f0431e4b68e86aa661a481db723339c58bff8
PavelRahman404 <paveltauker@gmail.com>
2024-10-07 22:57:50 +0600

    Creating Delete operation in controller

---
+++ Restaurants.Application/Restaurants/Commands/DeleteRestaurant/DeleteRestaurantCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.Application.Restaurants.Commands.DeleteRestaurant
+{
+    public class DeleteRestaurantCommandHandler(ILogger<DeleteRestaurantCommandHandler> logger,
+        IRepositoriesRestaurants repositoriesRestaurants) : IRequestHandler<DeleteRestaurantCommand,bool>
+    {
+        public async Task<bool> Handle(DeleteRestaurantCommand request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"Deleting restaurant with id: {request.Id}");
+            var restaurant = await repositoriesRestaurants.GetByIdAsync( request.Id);
+            if (restaurant == null)
+            return false;
+
+            await  repositoriesRestaurants.Delete(restaurant);
+            return true;
+        }
+    }
+}
